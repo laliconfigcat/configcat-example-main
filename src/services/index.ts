5363856522b53141543dad56e8954e4755222f9f
Lajos Szoke <lali@configcat.com>
2024-06-15 06:29:35 +0200

    polinterval

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
 import * as configcat from 'configcat-js-ssr';
 
-const configCatClient = configcat.getClient('configcat-sdk-1/tP3YCDSlzUeMVb36fHm4Fw/MehpaJ_x1UakimZRkRlvsA', configcat.PollingMode.AutoPoll, {logger: configcat.createConsoleLogger(configcat.LogLevel.Debug)});
+const configCatClient = configcat.getClient('configcat-sdk-1/tP3YCDSlzUeMVb36fHm4Fw/MehpaJ_x1UakimZRkRlvsA', configcat.PollingMode.AutoPoll, {pollIntervalSeconds: 1, logger: configcat.createConsoleLogger(configcat.LogLevel.Debug)});
 
 export const getFlag = async (flag: string) =>
   await configCatClient.getValueAsync(flag, false);
